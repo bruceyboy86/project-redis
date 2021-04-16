8fd956287886f1b8675d21efb85e075b1e9c5398
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-04-16 19:00:48 +0100

    adding base project structure

---
+++ src/main/java/com/alba/hackaton/projectredis/ProjectRedisApplication.java
@@ -0,0 +1,13 @@
+package com.alba.hackaton.projectredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alba/hackaton/projectredis/ProjectRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alba.hackaton.projectredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
